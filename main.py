781d5f7d86ac1a9b4d0eebeab8f113d2aaddc883
moussacode <samouh269@gmail.com>
2026-01-06 10:26:35 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,76 @@
+# Gestionnaire de Dépenses Simple
+
+depenses = []
+
+while True:
+    print("MENU")
+    print("1. Ajouter une depense")
+    print("2. Afficher les depenses")
+    print("3. Total des depenses")
+    print("4. Depenses par categorie")
+    print("5. Quitter")
+    
+    choix = input("Choix : ")
+    
+    if choix == "1":
+        montant = float(input("Montant : "))
+        categorie = input("Categorie : ")
+        description = input("Description : ")
+        
+        depense = {
+            "montant": montant,
+            "categorie": categorie,
+            "description": description
+        }
+        depenses.append(depense)
+        print("Votre depeencse est ajouter")
+    
+   
+    elif choix == "2":
+
+        print("tous les depence ")
+        for i in range(len(depenses)):
+            d = depenses[i]
+            print(f"{i+1}. {d['montant']} FCFA - {d['categorie']} - {d['description']}")
+    
+   
+    elif choix == "3":
+        total = 0
+        for depense in depenses:
+            total += depense["montant"]
+        print(f"Total : {total} FCFA")
+    
+    
+    elif choix == "4":
+        print("categorie")
+        categories = {}
+        categorie_tab=[]
+        for depense in depenses:
+            categorie_item = depense["categorie"]
+            
+            if categorie_item not in categorie_tab:
+                categorie_tab.append(categorie_item)
+        print(categorie_tab)
+
+        for depense in depenses:
+
+            for i in categorie_tab: 
+                if i == depense["categorie"] :
+                    td += depense["montant"]
+            
+            for d in categories[depense["categorie"]]:
+                print(f"{d['montant']} FCFA: {d['description']}")
+    
+
+
+
+        
+        
+        
+    
+
+    elif choix == "5":
+        print("Au revoir!")
+        break
+    else :
+        print('Choisir une option entre 1 et 5')
